c19621510e5b61315827bb42ba4bf67f934f2aa5
David <david.boeryd@gmail.com>
2018-06-03 22:19:10 +0200

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,43 @@
-mod Tree {
+mod tree {
+    use std::rc::Rc;
 
     pub struct Tree {
-        pub left: Option<Box<Tree>>,
-        pub right: Option<Box<Tree>>,
+        pub left: Option<Rc<Tree>>,
+        pub right: Option<Rc<Tree>>,
         pub key: String,
     }
 
     impl Tree {
-        fn insert(tree: &Option<Box<Tree>>, key: &str) -> (Tree) {
+        fn insert(tree: &Option<Rc<Tree>>, key: &str) -> (Option<Rc<Tree>>) {
             match tree {
-                None => Tree {
+                None =>  Some(Rc::new(Tree {
                     left: None,
                     right: None,
-                    key: key.to_string(),
-                },
-                Some(tree) => {
-                    let Void = match key.cmp(&tree.key) {
-                        Less => {insert(&tree.left, key)}
-                        More => {insert(&tree.right, key)}
+                    key: key.to_string()
+                })),
+                Some(&tree) => {
+                    let Void = match key.cmp(tree.key) {
+                        Less => Tree{
+                            left: Tree.insert(&tree.left, key),
+                            key: tree.key.clone(),
+                            right: Rc::clone(&tree.right)
+                        },
+                        More => Tree{
+                            left: Some(Rc::clone(tree.left.unwrap())),
+                            key: tree.key.clone(),
+                            right: insert(&tree.right, key)
+                        },
                         Equal => {}
                     };
-                    Tree {
-                        left: None,
-                        right: None,
-                        key: "fo".to_string(),
-                    }
                 }
             }
         }
+
+
     }
 }
 fn main() {
-    let tree = Tree::Tree {
+    let tree = tree::Tree {
         left: None,
         right: None,
         key: "ab".to_string(),
