300b40cfabaa51002fdcf946d5b7269f135bb0b5
David <david.boeryd@gmail.com>
2018-06-04 16:09:49 +0200

    stuff2

--- src/main.rs
+++ src/main.rs
@@ -10,28 +10,44 @@ mod tree {
     impl Tree {
         fn insert(tree: &Option<Rc<Tree>>, key: &str) -> (Option<Rc<Tree>>) {
             match tree {
-                None =>  Some(Rc::new(Tree {
-                    left: None,
-                    right: None,
-                    key: key.to_string()
-                })),
-                Some(&tree) => {
-                    let Void = match key.cmp(tree.key) {
-                        Less => Tree{
-                            left: Tree.insert(&tree.left, key),
-                            key: tree.key.clone(),
-                            right: Rc::clone(&tree.right)
-                        },
-                        More => Tree{
-                            left: Some(Rc::clone(tree.left.unwrap())),
-                            key: tree.key.clone(),
-                            right: insert(&tree.right, key)
-                        },
-                        Equal => {}
-                    };
+                None => new_leaf(),
+                Some(&tree) => tree_by_key(tree);
                 }
             }
         }
+
+        fn append_node(node: Option<Rc<Tree>>) -> (Option<Rc<Tree>>){
+            match node{
+                None => None,
+                Some(rc) => Some(Rc::clone(rc))
+
+            }
+        }
+        fn new_leaf(key: &str) -> (Option<Rc<Tree>>){
+            Some(Rc::new(Tree {
+                left: None,
+                right: None,
+                key: key.to_string()
+            }))
+        }
+
+        fn tree_by_key(&tree) ->( Option<Rc<Tree>>){
+            match key.cmp(tree.key) {
+                Less => Some(Rc::new(Tree{
+                    left: Tree.insert(&tree.left, key),
+                    key: tree.key.clone(),
+                    right: append_node(&tree.right)
+                })),
+                More => Some(Rc::new(Tree{
+                    left: append_node(&tree.left),
+                    key: tree.key.clone(),
+                    right: insert(&tree.right)
+                })),
+                Equal => {
+                    Rc::clone(:)
+                }
+
+    }
 
 
     }
